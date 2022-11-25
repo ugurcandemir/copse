2d91c49883702b460f447d545ed2b107d0236bc2
ugurcandemir <ugur.can.demir@hotmail.com>
2022-11-25 23:43:18 +0300

    test3

--- tests/testthat/test-chowTest.R
+++ tests/testthat/test-chowTest.R
@@ -1,3 +1,8 @@
+
+y <- c(5*rnorm(30 , 100 , 5 ) + 60 , 2*rnorm(20 , 100 , 5)+20)
+x <- 5*rnorm(50 , 100 , 5 ) + 20
+model1 <- lm(y ~ x)
+
 test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+  expect_lt(ChowTest(model1 , 30)$p_value , 0.05)
 })
