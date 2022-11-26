9a60bc3e4bd1ab20391db18eb44ffbfdda5c3bbb
ugurcandemir <ugur.can.demir@hotmail.com>
2022-11-26 14:31:06 +0300

    test5

--- tests/testthat/test-resetTest.R
+++ tests/testthat/test-resetTest.R
@@ -1,3 +1,13 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+set.seed(100)
+y <- rnorm(50 , 100 , 15)
+x <- runif(50 , 50 , 150 )
+model1 <- lm(y ~ x)
+
+test_that("Reset Test", {
+  expect_gt(ResetTest(model1 , 2 , "rss")$p_value , 0.10)
+  expect_lt(ResetTest(model1 , 2:3 , "rss")$p_value , 0.10)
+  expect_lt(ResetTest(model1 , 3:4 , "r.squared")$p_value , 0.10)
 })
+
+
